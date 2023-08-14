e1044eec2ae98da9903212a4d264d35fe8043e69
Nikolai Kolesnikov <zveroboy85@gmail.com>
2023-08-14 17:59:09 +0300

    fix error handling tests

--- test/error-handling.spec.ts
+++ test/error-handling.spec.ts
@@ -1,31 +1,28 @@
-// import { before, beforeEach, describe, it, mock } from 'node:test';
-import { Application } from '#bootstrap/application.js';
-import { container } from '#bootstrap/di.js';
-import { beforeAll, describe, expect, it, test } from '@jest/globals';
+import { ApplicationService, container } from '#application/index.js';
 import { StatusCodes } from 'http-status-codes';
 import assert from 'node:assert/strict';
+import { after, before, describe, it } from 'node:test';
 import request from 'supertest';
-import { DataSource, Repository } from 'typeorm';
 
-// import { app } from '../src/application.js';
-
-let userRepository: Repository<User>;
-let application: Application;
+let application: ApplicationService;
 
 describe('Error handling', () => {
-  beforeAll(async () => {
-    application = await Application.create(container);
-    userRepository = AppDataSource.getRepository(User);
+  before(async () => {
+    application = await container.getAsync<ApplicationService>(ApplicationService);
+  });
+
+  after(async () => {
+    await application.destroy();
   });
 
   it('should correctly respond if found', async () => {
-    const response = await request(app).get('/');
+    const response = await request(application.app).get('/');
     assert.equal(response.status, StatusCodes.OK);
-    assert.equal(response.text, 'foo bar');
+    assert.ok(response.text.startsWith('version '));
   });
 
   it('should correctly respond if not found', async () => {
-    const response = await request(app).get('/not-exists');
+    const response = await request(application.app).get('/not-exists');
     assert.equal(response.status, StatusCodes.NOT_FOUND);
   });
 });
